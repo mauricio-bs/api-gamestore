56a2387cda81ee2852444bee8d2d7f5382c73cf2
mauricio-bs <mauricio.3g2014@gmail.com>
2021-09-25 18:44:13 -0300

    Ordering models equal files order

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,30 @@
 import Sequelize from 'sequelize'
 import mongoose from 'mongoose'
 import dbConfig from '../config/database'
+//Models
+import Address from '../app/model/Address'
+import CardFlag from '../app/model/CardFlag'
+import Category from '../app/model/Category'
+import City from '../app/model/City'
+import Country from '../app/model/Country'
+import Payment from '../app/model/Payment'
+import PaymentType from '../app/model/PaymentType'
+import Product from '../app/model/Product'
+import State from '../app/model/State'
+import User from '../app/model/User'
 
-const models = []
+const models = [
+  User,
+  Product,
+  CardFlag,
+  Address,
+  City,
+  State,
+  Country,
+  Payment,
+  PaymentType,
+  Category,
+]
 
 class Database {
   constructor() {
