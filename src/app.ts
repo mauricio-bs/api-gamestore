0f879038dc1ee719cba0cae0f29f93d27fc946f4
mauricio-bs <mauricio.3g2014@gmail.com>
2021-12-27 21:26:20 -0300

    Configurado cors e socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
+import 'dotenv/config'
 import express from 'express'
 import { route } from './routes'
 import cors from 'cors'
-import './database'
 import http from 'http'
+import { Server } from 'socket.io'
 
 const app = express()
 
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'] }))
 const httpServer = http.createServer(app)
 
-app.use(cors())
+const io = new Server(httpServer)
+
+io.on('connect', socket => {
+  console.log('Connected on socket: ', socket)
+})
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(route)
 
-export { httpServer }
+export { httpServer, io }
